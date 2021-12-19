721a40053f8f75432306217e14f309188c2fe55d
Wiktor Górczak <wiktor.gorczak@openanalytics.eu>
2021-12-19 13:38:02 +0100

    initialize spring project

---
+++ src/main/java/pl/poznan/put/cs/net/testexample/TestexampleApplication.java
@@ -0,0 +1,13 @@
+package pl.poznan.put.cs.net.testexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/poznan/put/cs/net/testexample/TestexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.poznan.put.cs.net.testexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
